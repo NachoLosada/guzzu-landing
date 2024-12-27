3a22b2558a7d8f057d910d510ec250aef3a6badd
Ignasi Reixach <ignasireixach@gmail.com>
2024-12-27 11:35:19 +0100

    feat: add images, favicon and header gradient

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
+import guzzuLogo from "../../assets/images/guzzu-black-logo.png";
 import "./Header.css";
 
 const Header = () => {
 	return (
 		<header className="header">
-			<div className="logo">Logo placeholder</div>
+			<img className="logo" src={guzzuLogo} alt="Logo" />
 			<select className="language-selector">
 				<option value="en">English</option>
 				<option value="es">Español</option>
-				<option value="ca">Català</option>
 			</select>
 		</header>
 	);
