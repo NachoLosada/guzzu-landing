da9d8333f4566a328f35294952fb1d1315a4919e
Nacho Losada <ignaciolosadaprato@gmail.com>
2024-12-30 21:55:55 +0100

    feat: fix position an w h

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 		<div className="app-container">
 			<Header />
 			<Body />
-			{/* <Footer /> */}
+			<Footer />
 		</div>
 	);
 }
